6d7b2f054e7c7fbeaaf872d295e593e688b0de97
DevankGupta <devankgupta49@gmail.com>
2026-01-18 14:27:19 +0530

    Updated frontend build

--- src/main/java/com/email/writer/app/EmailGeneratorController.java
+++ src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -4,10 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/email")
-@AllArgsConstructor
 @CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
